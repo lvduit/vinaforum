6c4b1153d790f83a97f547622071a28e147f57e4
duyet2000@gmail.com <duyet2000@gmail.com@21b1cd37-add7-27e2-1533-26238932d6ca>
2012-06-13 16:12:11 +0000

    Add config file

---
+++ trunk/config.php
@@ -0,0 +1,24 @@
+<?php 
+if( !defined( 'IS_VF' ) ) die();
+
+$vF_Config = stdClass;
+
+# ------------------------------
+# SQL Infomation
+  $vF_Config->dbhost = 'localhost';
+  $vF_Config->dbuser = 'root';
+  $vF_Config->dbpass = '';
+  $vF_Config->dbname = 'vinaforum';
+# -------------------------------
+
+
+# -------------------------------
+# Developer Options
+  $vF_Config->debug = false;
+  $vF_Config->adminIpOnly = false;
+# -------------------------------
+
+# -------------------------------
+# Not Change
+  $vF_Config->key = '8300f1006560ab382348760502d3c760';
+# -------------------------------
