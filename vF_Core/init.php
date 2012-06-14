c7b646a71e3d8a1bbae15c6ebd8e560cc9853c34
duyet2000@gmail.com <duyet2000@gmail.com@21b1cd37-add7-27e2-1533-26238932d6ca>
2012-06-14 14:33:46 +0000

    Cau hinh lai init.

--- vF_Core/init.php
+++ vF_Core/init.php
@@ -7,28 +7,19 @@ vf_check();
 # ---------------------------
 if( $vF ) return;
 
-	$vF = stdClass;
-	$vF->init = $vF->User = $vF->Client = $vF->Options = $vF->Lang = stdClass;
+vF_config::getInstance()->loadConfig( vF_DIR . '/' . vF_constant::vF_CONFIG_FILE );
+if( !$vF_Config OR !is_object( $vF ) )
 
-	// Load config file
-	require( vF_DIR . '/' . vF_CONFIG_FILE );
-
-	$vF->init->time = $start_time;
-
-	$vF->System = new vF_system;
-	$vF->System->startSystem();
-
-	$vF->Input = new vF_input;
-
-	$vF->Client = vF_getClient::getInstance()->getClient();
-
-	$vF->Db = new vF_database(  );
-
-	$vF->User = vF_getUser::getInstance()->getUser();
-
-	$vF->Options = vF_getOptions::getInstance()->getOptions();
-
-	$vF->Lang = new vF_Language( $vF->Options->forumLanguage );
-
+global $vF_Config;
+global $vF;
+$vF = vF_init::getInstance();
+$vF->System = vF_system::getInstance();
+$vF->System->startSystem();
+$vF->Input = vF_input::getInstance();
+$vF->Client = vF_getClient::getInstance()->getClient();
+$vF->Db = vF_database::getInstance( $vF_Config );
+$vF->User = vF_getUser::getInstance()->getUser();
+$vF->Options = vF_getOptions::getInstance()->getOptions();
+$vF->Lang = vF_Language::getInstance();
 
 ?>
