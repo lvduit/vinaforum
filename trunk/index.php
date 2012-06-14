89908e56920e124df5e62abf195150fd4dac6649
duyet2000@gmail.com <duyet2000@gmail.com@21b1cd37-add7-27e2-1533-26238932d6ca>
2012-06-14 14:36:35 +0000

    Load he thong qua class vF_module.

--- trunk/index.php
+++ trunk/index.php
@@ -1,6 +1,12 @@
 <?php 
-// Show main forum
 define( 'IS_VF', true );
+$start_time = time();
 require( dirname( __file__ ) . '/global.php' );
-
-die('here');
+vF_module::getInstance()->loadModule(
+	vF_module::getInstance()->checkModule(
+		vF_input::getInstance()->getStringParam( 
+			vF_constant::vF_MODULE_PARAM, 'g', 'forum'
+		)
+	)
+);
+?>
