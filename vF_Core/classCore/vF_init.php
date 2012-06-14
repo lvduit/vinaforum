3d0bfefeaa8497102e1a38812b0f93dc3ea6c6d2
duyet2000@gmail.com <duyet2000@gmail.com@21b1cd37-add7-27e2-1533-26238932d6ca>
2012-06-14 14:27:02 +0000

    Cau hinh he thong

---
+++ vF_Core/classCore/vF_init.php
@@ -0,0 +1,36 @@
+<?php 
+vF_Check();
+
+# ----------------------
+# Class: Init
+# Author: Yplit
+# Date: 14/6/2012
+#-----------------------
+class vF_init
+{
+	private $_instance;
+	public $User;
+	public $Client;
+	public $Options;
+	public $Lang;
+
+	public function __construct()
+	{
+	}
+
+	public function getInstance()
+	{
+		if( !self::$_instance )
+		{
+			self::$_instance = new self();
+		}
+
+		return self::$_instance;
+	}
+
+	public function newParam( $paramName, $paramValue = stdClass )
+	{
+		$this->$paramName = $paramValue;
+	}
+
+}
