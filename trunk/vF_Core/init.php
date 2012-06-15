d496003700af50110bcf3266fffc49820d4c3c1c
duyet2000@gmail.com <duyet2000@gmail.com@21b1cd37-add7-27e2-1533-26238932d6ca>
2012-06-15 15:34:07 +0000

    Fix loadConfig

--- trunk/vF_Core/init.php
+++ trunk/vF_Core/init.php
@@ -3,22 +3,22 @@ vf_check();
 
 # ---------------------------
 # Global Init
-# Note: Start System > Input > Client >  Db > Options > User >  Lang
 # ---------------------------
 if( $vF ) return;
 
-vF_config::getInstance()->loadConfig( vF_DIR . '/' . vF_constant::vF_CONFIG_FILE );
-if( !$vF_Config OR !is_object( $vF ) )
+vF_config::getInstance()->loadConfig( vF_DIR . '/' . vF_constant::vF_CONFIG_FILE, $vF_Config );
 
-global $vF_Config;
+global $vF_Config; 
+
 global $vF;
 $vF = vF_init::getInstance();
 $vF->System = vF_system::getInstance();
 $vF->System->startSystem();
+$vF->System->debug = ( $vF_Config->debug !== true ? false : true );
 $vF->Input = vF_input::getInstance();
 $vF->Client = vF_getClient::getInstance()->getClient();
 $vF->Db = vF_database::getInstance( $vF_Config );
-$vF->User = vF_getUser::getInstance()->getUser();
+//$vF->User = vF_getUser::getInstance()->getUser();
 $vF->Options = vF_getOptions::getInstance()->getOptions();
 $vF->Lang = vF_Language::getInstance();
 
