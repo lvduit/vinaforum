8cf7994b1b40682db40664d53ee530d3ef92f9eb
duyet2000@gmail.com <duyet2000@gmail.com@21b1cd37-add7-27e2-1533-26238932d6ca>
2012-06-16 10:39:18 +0000

    Quy dinh lai cau truc cua module

---
+++ trunk/vF_Core/module/forum/isModule.php
@@ -0,0 +1,21 @@
+<?php 
+vF_Check();
+
+# ----------------------
+# File: Check Module & Module Info
+# Author: Yplit
+# Date: 14/6/2012
+#-----------------------
+$vF_moduleInfo = array(
+	'moduleName' => 'forum',
+	'version' => '1.0',
+	'author' => 'vF',
+	'date' => '16/6/2012',
+	'description' => 'Main Forum',
+);
+
+$vF_moduleOptions = array(
+	'main',
+);
+
+$vF_moduleOptionsDefault = 'main';
